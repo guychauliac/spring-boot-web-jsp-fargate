fc196fe0cd15998ed1b529dfcd7030318f135788
Guy Chauliac <guy.chauliac@axa.be>
2019-07-15 09:53:20 +0200

    openshift

--- docker/exportconfig.cmd
+++ docker/exportconfig.cmd
@@ -1,3 +1,3 @@
 oc export buildconfig spring-boot-web-jsp -o yaml > buildconfig.yaml
 oc export deploymentconfig spring-boot-web-jsp -o yaml > deployconfig.yaml
-
+oc export service spring-boot-web-jsp -o yaml > serviceconfig.yaml
